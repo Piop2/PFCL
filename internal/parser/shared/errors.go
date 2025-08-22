635744e41b7c54cb7095c0466c205549969001f3
piop2 <pioooop2@gmail.com>
2025-08-23 00:41:50 +0900

    refactor: unify error message format

--- internal/parser/shared/errors.go
+++ internal/parser/shared/errors.go
@@ -39,5 +39,5 @@ type ErrSyntax struct {
 func (e *ErrSyntax) SetMessage(msg string) { e.Message = msg }
 func (e *ErrSyntax) SetPos(line, col int)  { e.Pos = [2]int{line, col} }
 func (e *ErrSyntax) Error() string {
-	return fmt.Sprintf("syntax error [%d:%d]: %s", e.Pos[0], e.Pos[1], e.Message)
+	return fmt.Sprintf("[%d:%d] syntax error: %s", e.Pos[0], e.Pos[1], e.Message)
 }
